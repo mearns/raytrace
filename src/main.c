c76228ecef8acde55f45b3b3f7ddc85d876ce141
Brian Mearns <bmearns@ieee.org>
2014-10-23 12:30:06 -0400

    Got simple GTK+ pixmap working.
    We can now actually draw to the screen.

--- src/main.c
+++ src/main.c
@@ -6,12 +6,43 @@
 
 #include <gtk/gtk.h>
 #include <gdk/gdk.h>
+#include <gdk-pixbuf/gdk-pixbuf.h>
 
 #include "triangle.h"
 #include "point.h"
 #include "vertex.h"
 #include "color.h"
 
+static const int WIDTH = 200;
+static const int HEIGHT = 200;
+
+gboolean expose_event_callback (GtkWidget *widget, GdkEventExpose *event, gpointer data)
+{
+    unsigned int i, j;
+
+    GdkPixbuf * pixbuf = gdk_pixbuf_new(GDK_COLORSPACE_RGB, FALSE, 8, WIDTH, HEIGHT);
+    g_assert(gdk_pixbuf_get_n_channels(pixbuf) == 3);
+    g_assert(gdk_pixbuf_get_width(pixbuf) == WIDTH);
+    g_assert(gdk_pixbuf_get_height(pixbuf) == HEIGHT);
+
+    
+    const int rowstride = gdk_pixbuf_get_rowstride (pixbuf);
+    guchar *const pixels = gdk_pixbuf_get_pixels (pixbuf);
+    guchar *pix;
+    for(i=0; i<WIDTH; i++) {
+        for(j=0; j<HEIGHT; j++) {
+            pix = pixels + (j*rowstride) + (i*3);
+            pix[0] = i % 255; //red
+            pix[1] = j % 255; //green
+            pix[2] = 0; //blue;
+            printf("%d,%d\n", i, j);
+        }
+    }
+
+    gdk_draw_pixbuf(widget->window, NULL, pixbuf, 0, 0, 0, 0, WIDTH, HEIGHT, GDK_RGB_DITHER_NONE, 0, 0);
+
+    return TRUE;
+}
 
 int main(int argc, char **argv)
 {
@@ -24,6 +55,7 @@ int main(int argc, char **argv)
 
     /* Create a new window, give it a title and display it to the user. */
     window = gtk_window_new (GTK_WINDOW_TOPLEVEL);
+    gtk_window_set_default_size(GTK_WINDOW(window), WIDTH, HEIGHT);
     gtk_window_set_title (GTK_WINDOW (window), "Hello World");
     gtk_widget_show (window);
 
@@ -40,6 +72,12 @@ int main(int argc, char **argv)
 
     printf("Area of triangle: %f\n", triangle->area);
     printf("Triangle at (%f, %f, %f) is [%d,%d,%d]\n", test_pt->x, test_pt->y, test_pt->z, test_color.r, test_color.g, test_color.b);
+
+    //GtkWidget *darea = gtk_drawing_area_new();
+    //gtk_widget_set_size_request(darea, WIDTH, HEIGHT);
+    g_signal_connect(G_OBJECT(window), "expose_event", G_CALLBACK(expose_event_callback), NULL);
+
+    //gtk_container_add(GTK_CONTAINER(window), darea);
 
     /* Hand control over to the main loop. */
     gtk_main ();
