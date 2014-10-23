d5fa37d3eaae9fc9090de97001f8d9c3d4284bbc
Brian Mearns <bmearns@ieee.org>
2014-10-23 08:58:24 -0400

    Started building with GTK+
    - Got SConstruct set up to use pkg-config to set options for building
      with installed GTK+.
    - Launching a simply GTK window in main. It doens't do anything yet.

--- src/main.c
+++ src/main.c
@@ -4,13 +4,29 @@
  */
 #include <stdio.h>
 
+#include <gtk/gtk.h>
+#include <gdk/gdk.h>
+
 #include "triangle.h"
 #include "point.h"
 #include "vertex.h"
 #include "color.h"
 
 
-int main(void) {
+int main(int argc, char **argv)
+{
+
+    GtkWidget *window;
+
+    /* Initialize the GTK+ and all of its supporting libraries. */
+    gtk_init (&argc, &argv);
+    gdk_init (&argc, &argv);
+
+    /* Create a new window, give it a title and display it to the user. */
+    window = gtk_window_new (GTK_WINDOW_TOPLEVEL);
+    gtk_window_set_title (GTK_WINDOW (window), "Hello World");
+    gtk_widget_show (window);
+
 
     Vertex_t *vert_a = Vertex(Point(0, 0, 0), Color(255, 0, 0));
     Vertex_t *vert_b = Vertex(Point(0, 1, 0), Color(0, 255, 0));
@@ -25,6 +41,8 @@ int main(void) {
     printf("Area of triangle: %f\n", triangle->area);
     printf("Triangle at (%f, %f, %f) is [%d,%d,%d]\n", test_pt->x, test_pt->y, test_pt->z, test_color.r, test_color.g, test_color.b);
 
+    /* Hand control over to the main loop. */
+    gtk_main ();
     return 0;
 }
 
