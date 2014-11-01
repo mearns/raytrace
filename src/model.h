9c1c4a69d7655eb2101d8f184d836dee9dc69cb1
Brian Mearns <bmearns@ieee.org>
2014-10-31 21:37:53 -0400

    Added axes-delegate prototypes for model.h

--- src/model.h
+++ src/model.h
@@ -18,6 +18,20 @@ struct Model {
 
 Model_t * Model_cfg(Model_t *pThis);
 
+Model_t * Model_yaw(Model_t *const pThis, const double rads);
+
+Model_t * Model_pitch(Model_t *const pThis, const double rads);
+
+Model_t * Model_roll(Model_t *const pThis, const double rads);
+
+Model_t * Model_march(Model_t *const pThis, const double dist);
+
+Model_t * Model_strafe(Model_t *const pThis, const double dist);
+
+Model_t * Model_climb(Model_t *const pThis, const double dist);
+
+
+
 #endif
 //end inclusion filter
 
